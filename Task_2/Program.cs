ac69e5ebfbe75d43b6bf9019335ce79400a813d1
Yan <yangalperin@gmail.com>
2022-08-30 08:57:37 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1 7 -> такого числа в массиве нет
+
+
+
+int[,] matr = FillMatrix(3, 4, -10, 10);
+
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+PrintMatrix(matr);
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(m < 0 || m > matr.GetLength(0)-1 || n < 0 || n > matr.GetLength(1)-1)
+{
+    Console.WriteLine("Такого элемента массива нет");
+}
+else
+{
+Console.WriteLine(matr.GetValue(m,n));
+}
+
+
+
+
